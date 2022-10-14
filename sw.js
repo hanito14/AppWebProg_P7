64ea7b16ea593889166d6c29d7c19b80da810ede
= <jeanhernandez@utez.edu.mx>
2022-10-14 09:03:17 -0500

    Cambios xd

--- sw.js
+++ sw.js
@@ -20,16 +20,16 @@ self.addEventListener('install',(event)=>{
 
     const respCache = caches.open('cache-v1').then((cache)=>{
         return cache.addAll([
-            '/',
-            '/index.html',
-            '/manifest.json',
-            '/images/icons/android-launchericon-48-48.png',
-            '/images/icons/android-launchericon-72-72.png',
-            '/images/icons/android-launchericon-96-96.png',
-            '/images/icons/android-launchericon-144-144.png',
-            '/images/icons/android-launchericon-192-192.png',
-            '/images/icons/android-launchericon-512-512.png',
-            '/images/Spider-Man.jpg',
+            './',
+            './index.html',
+            './manifest.json',
+            './images/icons/android-launchericon-48-48.png',
+            './images/icons/android-launchericon-72-72.png',
+            './images/icons/android-launchericon-96-96.png',
+            './images/icons/android-launchericon-144-144.png',
+            './images/icons/android-launchericon-192-192.png',
+            './images/icons/android-launchericon-512-512.png',
+            './images/Spider-Man.jpg',
             'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css',
             'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
             'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
